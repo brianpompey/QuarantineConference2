587a5bcbc1c4f6cd12147f436e54de13da06658f
brianpompey <brianthescholar@gmail.com>
2020-04-18 21:58:30 -0400

    wrote some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'conference#index'
+
+  resources :users do
+    resources :user_interests
+  end
+
+  resources :conferences do
+    resources :workshops
+  end
+
+  get '/signin', to: 'session#new', as: 'signin'
+
+  post '/session', to: 'session#create', as: 'session'
+
+  delete '/session/', to: 'session#destroy'
+  
 end
