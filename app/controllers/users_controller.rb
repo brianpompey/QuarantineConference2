6ccbd44a3c81a3c6d0068a0dda8a0c21390737bd
brianpompey <brianthescholar@gmail.com>
2020-04-19 22:30:20 -0400

    made new conference form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     def create
         @user = User.create(user_params)
         session[:user_id] = @user.id
-        redirect_to new_user_user_interest_path(@user)
+        redirect_to edit_user_interest_path(@user)
     end
 
 
