fea3b1a9cfdfbb48095a3dcf0e2d5be704b2211b
brianpompey <brianthescholar@gmail.com>
2020-04-20 17:26:46 -0400

    finally adding sessions, need to finish up the controller actions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+
+    def destroy
+        session.delete("user_id")
+        redirect_to root_path
+    end
+end
