5f8f58fc8a6373a96d16ead7713d17022ec4700d
brianpompey <brianthescholar@gmail.com>
2020-04-19 21:23:35 -0400

    user signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'conference#index'
+  root 'conferences#index'
 
+  get '/signup', to: 'users#new'
+  
   resources :users do
     resources :user_interests
   end
@@ -16,5 +18,5 @@ Rails.application.routes.draw do
   post '/session', to: 'session#create', as: 'session'
 
   delete '/session/', to: 'session#destroy'
-  
+
 end
