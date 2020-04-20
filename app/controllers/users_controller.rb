8712482e09de9d1d762b15a899dec21fc3761a8c
brianpompey <brianthescholar@gmail.com>
2020-04-19 22:35:51 -0400

    comment out session stuff for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
     def create
         @user = User.create(user_params)
-        session[:user_id] = @user.id
+     #   session[:user_id] = @user.id
         redirect_to edit_user_interest_path(@user)
     end
 
