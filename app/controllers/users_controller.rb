52f55b44bc001edc849c20d1d4bfc68c7c0898a8
brianpompey <brianthescholar@gmail.com>
2020-04-24 17:47:14 -0400

    got sessions to work, now have to figure out the root

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
     def create
         @user = User.create(user_params)
         session[:user_id] = @user.id
-        redirect_to "/users/#{@user.id}/user_interests/new"
+        redirect_to root_url
+    #    redirect_to "/users/#{@user.id}/user_interests/new"
     end
 
     def show
