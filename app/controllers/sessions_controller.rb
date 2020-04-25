85f90f20011e88ed0e5d793099f97f5d95bc5943
brianpompey <brianthescholar@gmail.com>
2020-04-24 21:09:50 -0400

    trying to figure out the omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,21 @@ class SessionsController < ApplicationController
 
     def create
     #    byebug
-        @user = User.find_by_email(params[:email])
-        if @user && @user.authenticate(params[:password])
-            session[:user_id] = @user.id
+        if auth_hash = request.env["omniauth.auth"]
+            user = User.from_omniauth(auth_hash)
+            session[:user_id] = user.id
             redirect_to root_path
+        
         else
-            flash.now[:alert] = "Email or password is invalid"
-            render "new"
+
+            @user = User.find_by_email(params[:email])
+            if @user && @user.authenticate(params[:password])
+                session[:user_id] = @user.id
+                redirect_to root_path
+            else
+                flash.now[:alert] = "Email or password is invalid"
+                render "new"
+            end
         end
     end
 
