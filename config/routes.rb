0d5d4af1fab55f049163aea5e48a6bec71e0811c
brianpompey <brianthescholar@gmail.com>
2020-04-28 17:13:19 -0400

    seeded the database

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Rails.application.routes.draw do
 
   post '/sessions', to: 'sessions#create', as: 'session'
 
-  delete '/session/', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 
   post '/donations', to: 'donations#create'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,45 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Conference.delete_all
 
-Conference.create(:name => "RubyCon", :min_donation => 25, :date => "2020-06-05", :time => "16:00", :keynote => "Yukihiro Matsumoto", :category => "Ruby Programming")
+Conference.create(:name => "RubyCon", :min_donation => 25, :date => "2020-06-05", :time => "16:00", :keynote => "Yukihiro Matsumoto", :category => "Ruby")
 
-Conference.create(:name => "RailsAThon", :min_donation => 35, :date => "2020-08-05", :time => "12:00", :keynote => "David Heinemeier Hansson", :category => "Ruby on Rails")
+Conference.create(:name => "RailsAThon", :min_donation => 35, :date => "2020-08-05", :time => "12:00", :keynote => "David Heinemeier Hansson", :category => "Ruby")
 
 Conference.create(:name => "We Love Python!!", :min_donation => 0, :date => "2020-09-02", :time => "13:00", :keynote => "Guido van Rossum", :category => "Python")
 
 Conference.create(:name => "Javaaaaaascript!", :min_donation => 50, :date => "2020-11-03", :time => "10:00", :keynote => "Brendan Eich", :category => "Javascript")
 
-Conference.create(:name => "React to THIS", :min_donation => 25, :date => "2020-12-15", :time => "14:00", :keynote => "Jordan Walke", :category => "React JS")
+Conference.create(:name => "React to THIS", :min_donation => 25, :date => "2020-12-15", :time => "14:00", :keynote => "Jordan Walke", :category => "Javascript")
+
+Conference.create(:name => "Python Partay!", :min_donation => 25, :date => "2020-09-15", :time => "17:00", :keynote => "Alex Martelli", :category => "Python")
+
+Conference.create(:name => "Ruby at Flatiron", :min_donation => 0, :date => "2020-12-05", :time => "12:30", :keynote => "Avi Flombaum", :category => "Ruby")
+
+Conference.create(:name => "NodeCON", :min_donation => 40, :date => "2020-05-02", :time => "12:30", :keynote => "Ryan Dahl", :category => "Javascript")
+
+Conference.create(:name => "All About Sinatra", :min_donation => 50, :date => "2020-11-15", :time => "16:30", :keynote => "Blake Mizerany", :category => "Ruby")
+
+Conference.create(:name => "REDUX", :min_donation => 40, :date => "2020-10-15", :time => "15:00", :keynote => "Dan Abramov", :category => "Javascript")
 
 
 Workshop.delete_all
 
-Workshop.create(:name => "Matz", :description => "OO Ruby Tic Tac Toe", :confernce_id => 1)
+Workshop.create(:name => "Matz", :description => "OO Ruby Tic Tac Toe", :conference_id => 1)
 
-Workshop.create(:name => "Jason Fried", :description => "Rails Authentication", :confernce_id => 2)
+Workshop.create(:name => "Jason Fried", :description => "Rails Authentication", :conference_id => 2)
 
-Workshop.create(:name => "Damien P. George", :description => "MicroPython", :confernce_id => 3)
+Workshop.create(:name => "Damien P. George", :description => "MicroPython", :conference_id => 3)
 
-Workshop.create(:name => "Douglas Crockford", :description => "JSON", :confernce_id => 4)
+Workshop.create(:name => "Douglas Crockford", :description => "JSON", :conference_id => 4)
 
-Workshop.create(:name => "Mark Zuckerberg", :description => "React Native Benefits", :confernce_id => 5)
+Workshop.create(:name => "Mark Zuckerberg", :description => "React Native Benefits", :conference_id => 5)
+
+Workshop.create(:name => "Robert Bradshaw", :description => "What Cython is All About", :conference_id => 6)
+
+Workshop.create(:name => "Avi", :description => "Getting Started with Ruby", :conference_id => 7)
+
+Workshop.create(:name => "Timothy J. Fontaine", :description => "Leading Node", :conference_id => 8)
+
+Workshop.create(:name => "Trygve Reenskaug", :description => "Sinatra MVC Structure", :conference_id => 9)
+
+Workshop.create(:name => "Andrew Clark", :description => "All the Angles of Angular", :conference_id => 10)
