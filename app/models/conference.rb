9eca5c3741f23fbf67426e5e5b06deb65846376f
brianpompey <brianthescholar@gmail.com>
2020-04-28 21:16:03 -0400

    conference show page and current user logic for it

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -3,4 +3,14 @@ class Conference < ApplicationRecord
     has_many :users, through: :donations
     has_many :workshops
     accepts_nested_attributes_for :workshops, reject_if: lambda {|attributes| attributes['name'].blank?}
+
+
+    def self.search(search)
+        if search
+
+
+        else
+            @conferences = Conference.all
+        end
+    end
 end
